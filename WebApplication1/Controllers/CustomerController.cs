1e942204a76c929b45e11d627d598a001fc369ab
朱炳燁 Ian <ian.chu@miniasp.com>
2018-07-11 18:33:56 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using ClosedXML.Excel;
+using ClosedXML.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerRepository repo = new CustomerRepository();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetExcel()
+        {
+            using (var wb = GetExcelFile())
+            {
+                // Add ClosedXML.Extensions in your using declarations
+                return wb.Deliver("generatedfile.xlsx");
+
+            }
+        }
+
+        private XLWorkbook GetExcelFile()
+        {
+            return ClosedXmlHelper.ToClosedXmlExcel(repo.GetCustomers());
+        }
+    }
+}
---
+++ WebApplication1/Models/CustomerRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class CustomerRepository
+    {
+        public List<Customer> GetCustomers()
+        {
+            return new List<Customer>() {
+                new Customer(){ Id=1,Name="王小明1",Phone="0912345678"},
+                new Customer(){ Id=2,Name="王小明2",Phone="0912345678"},
+                new Customer(){ Id=3,Name="王小明3",Phone="0912345678"},
+                new Customer(){ Id=4,Name="王小明4",Phone="0912345678"},
+                new Customer(){ Id=5,Name="王小明5",Phone="0912345678"},
+                new Customer(){ Id=6,Name="王小明6",Phone="0912345678"},
+            };
+        }
+    }
+}
